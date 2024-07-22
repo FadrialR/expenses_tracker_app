4a661bab6a53bbc3561e6d200e479b831f094517
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2024-07-22 18:09:13 +0700

    Create transactions page

---
+++ lib/screens/home/stats/stats.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class StatScreen extends StatelessWidget {
+  const StatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        child: Column(
+          children: [
+            Text(
+              'Transaction',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
