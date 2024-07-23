29133ef1205e4d68e784ad20960edab19b62bb0b
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2024-07-23 19:44:31 +0700

    Create chart

--- lib/screens/home/stats/stats.dart
+++ lib/screens/home/stats/stats.dart
@@ -1,3 +1,5 @@
+import 'package:expenses_tracker/screens/home/stats/chart.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class StatScreen extends StatelessWidget {
@@ -5,15 +7,27 @@ class StatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Transaction',
+            const Text(
+              'Transactions',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            )
+            ),
+            const SizedBox(height: 20),
+            Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12)),
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(12, 20, 12, 12),
+                  child: MyChart(),
+                ))
           ],
         ),
       ),
